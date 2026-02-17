767b9adb164dfd15edb7c2ce8d921fb317f43df0
Dwijdc07 <dwijchawla2002@gmail.com>
2026-02-17 23:23:20 +0530

    add new

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    role_choice = (
+        ('senior', 'Senior'),
+        ('junior', 'Junior'),
+    )
+    role = models.CharField(
+        choices=role_choice,
+        max_length=20,
+        default='junior'
+    )
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+
+
 class Tasks(models.Model):
     status_choice = (
         ('pending','Pending'),
@@ -11,12 +30,12 @@ class Tasks(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     assign_by = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         on_delete= models.CASCADE,
         related_name='assign_by'
     )
     assign_to = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         on_delete = models.CASCADE,
         related_name='assign_to' 
     )
